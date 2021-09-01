d94bd6c111d53c60952564f3bdfbb449febfe9a0
Raza Rajwani <r.rajwani@UBT.EDU.SA>
2021-09-01 12:05:50 +0300

    Initial Commit

---
+++ AuthDemo/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthDemo.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult login(string uid, string pwd)
+        {
+            bool status = false;
+            
+            if (uid.ToLower() == "admin" && pwd == "123")
+            {
+                var userClaims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name,uid),
+                };
+
+                var userIdentity = new ClaimsIdentity(userClaims, "User Identiy");
+                var userPrinciple = new ClaimsPrincipal(new[] { userIdentity });
+
+                HttpContext.SignInAsync(userPrinciple);
+                status = userPrinciple.Identity.IsAuthenticated;
+            }
+
+            return Json(new { status });
+        }
+
+
+        public IActionResult logout()
+        {
+            HttpContext.SignOutAsync();
+            return RedirectToAction("login");
+        }
+
+
+    }
+}
